b012d7c93d952634f9eb1f8a2d54bc9a595895ea
duplin anton <duplin.anton@gmail.com>
2021-08-18 15:31:41 +0300

    тест 2

---
+++ CoreDataDemo/StorageManager.swift
@@ -0,0 +1,60 @@
+//
+//  StorageManager.swift
+//  CoreDataDemo
+//
+//  Created by Anton Duplin on 17/8/21.
+//
+
+import Foundation
+import CoreData
+
+class StorageManager {
+    var taskListData: [Task] = []
+    
+    var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CoreDataDemo")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    static let shared = StorageManager()
+    
+    var viewContex: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    private init() {
+        
+    }
+
+    func fetchData() {
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
+        
+        do {
+            taskListData = try persistentContainer.viewContext.fetch(fetchRequest)
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+  
+    
+}
+
